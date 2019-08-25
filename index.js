7c8802e61a0af0249cf6a1232165a7e474eeeb26
abhisheksonnakula <sonnakulaabhishek98@gmail.com>
2019-08-25 13:57:48 +0530

    index.js commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const expressEdge = require('express-edge');
+const mongoose = require('mongoose')
+const fileUpload = require("express-fileupload");
+const expressSession = require("express-session");
+const connectMongo = require("connect-mongo");
+
+
+const app = new express();
+mongoose.connect("mongodb://localhost/romp", { useNewUrlParser: true });
+
+const mongoStore = connectMongo(expressSession);
+
+app.use(
+  expressSession({
+    secret: "secret",
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection
+    }),
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(expressEdge);
+app.set("views", `${__dirname}/views`);
+
+
+app.use("*", (req, res, next) => {
+  edge.global("auth", req.session.userId);
+  next();
+});
+
+
+app.listen(4000, () => {
+    console.log("App listening on port 4000");
+  });
